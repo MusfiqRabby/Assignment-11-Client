5d7db67bbdb562d3e9acbd9ca9d2aec9499852f9
musfiqrabby <musfiqrabby69@gmail.com>
2024-05-12 13:03:39 +0600

    statict card added

--- src/components/Home/Feature.jsx
+++ src/components/Home/Feature.jsx
@@ -5,7 +5,7 @@ const Feature = () => {
       <div className="mt-16 font-bold text-center hover:underline font-poppins text-4xl mb-8">
         <h2>Highlighted <span className="text-red-500">Features</span></h2>
      </div>
-     <div className="flex gap-16 mt-4 mb-16  ">
+     <div className="grid grid-cols-3 gap-16 mt-4 mb-16  ">
     <div className="card w-96 bg-grray-100 shadow-xl hover:scale-[1.05] transition-all">
   <figure>
     <img src="https://i.ibb.co/Khn8VYm/back-school-concept-with-slate-leaning-against-wall.jpg" alt="" />
@@ -45,6 +45,51 @@ const Feature = () => {
     Read Instructions Carefully
     </h2>
     <p>Before starting the test, carefully read through all instructions and questions. Allocate your time wisely, focusing on questions you feel confident about first before tackling more challenging ones</p>
+    <div className="card-actions justify-end">
+      <div className="badge badge-outline">Group</div> 
+      <div className="badge badge-outline">Discuss</div>
+    </div>
+  </div>
+</div>
+    <div className="card w-96 bg-base-100 shadow-xl hover:scale-[1.05] transition-all">
+  <figure>
+    <img className="w-full h-[250px]" src="https://i.ibb.co/VT9zcDV/55752.jpg" alt="" />
+    </figure>
+  <div className="card-body">
+    <h2 className="card-title">
+    Create an Infographic
+    </h2>
+    <p>In todays fast-paced world, where information overload is a common challenge, the need for effective communication has never been greater. Visual communication, in the form of infographics.</p>
+    <div className="card-actions justify-end">
+      <div className="badge badge-outline">Group</div> 
+      <div className="badge badge-outline">Discuss</div>
+    </div>
+  </div>
+</div>
+    <div className="card w-96 bg-base-100 shadow-xl hover:scale-[1.05] transition-all">
+  <figure>
+    <img  src="https://i.ibb.co/JQ3k5zN/books-vase-vintage-camera-eyeglasses-pencil-holders-white-blank-paper-wooden-backdrop.jpg" alt="" />
+    </figure>
+  <div className="card-body">
+    <h2 className="card-title">
+    Implement Responsive Design
+    </h2>
+    <p>In an era dominated by mobile devices, responsive design has become imperative for ensuring a seamless user experience across various screen sizes and devices. This guide explores the principles and techniques of .</p>
+    <div className="card-actions justify-end">
+      <div className="badge badge-outline">Group</div> 
+      <div className="badge badge-outline">Discuss</div>
+    </div>
+  </div>
+</div>
+    <div className="card w-96 bg-base-100 shadow-xl hover:scale-[1.05] transition-all">
+  <figure>
+    <img className="w-full h-[250px]" src="https://i.ibb.co/qDPKsHV/kae-ng-jy-Ncew0-Nf-W0-unsplash.jpg" alt="" />
+    </figure>
+  <div className="card-body">
+    <h2 className="card-title">
+    Beautifull Design
+    </h2>
+    <p>arefully read through all instructions and questions. Allocate your time wisely various screen sizes and devices. This guide explores the principles and techniques of .</p>
     <div className="card-actions justify-end">
       <div className="badge badge-outline">Group</div> 
       <div className="badge badge-outline">Discuss</div>
